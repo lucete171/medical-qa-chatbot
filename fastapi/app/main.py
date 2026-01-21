c741ccca5115db9f8c5f5b298bbaff4aadb8afc8
uamcode <tladbwls70@naver.com>
2026-01-21 16:18:57 +0900

    fastapi 수정

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app.api.health import router as health_router
 from app.api.inference import router as Inference_router
 from app.api.ws import router as ws_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app=FastAPI()
 
@@ -12,3 +13,11 @@ def road_root():
 app.include_router(health_router)  #app/api/health
 app.include_router(Inference_router)
 app.include_router(ws_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[""],  # 테스트 단계
+    allow_credentials=True,
+    allow_methods=[""],
+    allow_headers=["*"],
+)
